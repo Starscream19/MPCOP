3c37153bfb3178dc5a542e5b72e0d755c3d38acb
SatyamPal7 <satyam.mj7@gmail.com>
2020-07-22 15:23:22 +0530

    image uplaod with fir added

--- app/src/main/java/com/example/virtualpolicestation/FIR.java
+++ app/src/main/java/com/example/virtualpolicestation/FIR.java
@@ -1,14 +1,16 @@
 package com.example.virtualpolicestation;
 
 import android.content.Intent;
+
 import android.os.Bundle;
+
 import android.text.TextUtils;
 import android.util.Base64;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ProgressBar;
+
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -19,8 +21,15 @@ import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.FirebaseFirestoreSettings;
-import com.google.firebase.firestore.SetOptions;
+
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import java.util.UUID;
+
+import android.net.Uri;
+
 
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
@@ -37,7 +46,15 @@ public class FIR extends AppCompatActivity {
 
 
     EditText uFullName,uAddress,uCity,uPostcode,uSubject,uComplaint;
-    Button Register_FIR;
+    Button Register_FIR,btn_choose;
+
+
+
+    StorageReference storageReference;
+    FirebaseStorage storage;
+    private final int PICK_IMAGE_REQUEST = 22;
+    private Uri filePath;
+
 
 
     private FirebaseAuth fAuth;
@@ -73,15 +90,34 @@ public class FIR extends AppCompatActivity {
         uSubject   = findViewById(R.id.subject);
         uComplaint  = findViewById(R.id.Complaint);
         Register_FIR = findViewById(R.id.btn_fir);
+        btn_choose = findViewById(R.id.choose);
+
 
 
         fAuth = FirebaseAuth.getInstance();
         fStore = FirebaseFirestore.getInstance();
+        storage = FirebaseStorage.getInstance();
+        storageReference = storage.getReference();
+
+
+
+        btn_choose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                SelectImage();
+            }
+        });
+
 
 
         Register_FIR.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+
+
+
 
                 final String complaint = uComplaint.getText().toString();
 
@@ -98,7 +134,7 @@ public class FIR extends AppCompatActivity {
                     secretKey = keyGenerator.generateKey();
                     secretKeyen = secretKey.getEncoded();
                     strSecretKey = encoderfun(secretKeyen);
-//                    tvSecretKey.setText(strSecretKey);
+
 
                     random = new SecureRandom();
                     random.nextBytes(IV);
@@ -123,6 +159,12 @@ public class FIR extends AppCompatActivity {
                         String cipherSub = encoderfun(cipherSubject);
                         cipherPostcode = Encrypt.encrypt(uPostcode.getText().toString().trim().getBytes(),secretKey,IV);
                         String cipherPost = encoderfun(cipherPostcode);
+
+
+
+
+
+                        uploadImage();
 
 
 
@@ -195,8 +237,110 @@ public class FIR extends AppCompatActivity {
 
     }
 
+    private void SelectImage() {
 
-    public static String encoderfun(byte[] decval) {
+
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(
+                Intent.createChooser(
+                        intent,
+                        "Select Image from here..."),
+                PICK_IMAGE_REQUEST);
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode,
+                                    int resultCode,
+                                    Intent data)
+    {
+
+        super.onActivityResult(requestCode,
+                resultCode,
+                data);
+
+        // checking request code and result code
+        // if request code is PICK_IMAGE_REQUEST and
+        // resultCode is RESULT_OK
+        // then set image in the image view
+        if (requestCode == PICK_IMAGE_REQUEST
+                && resultCode == RESULT_OK
+                && data != null
+                && data.getData() != null) {
+
+            // Get the Uri of data
+            filePath = data.getData();
+
+        }
+    }
+
+
+
+    // UploadImage method
+    private void uploadImage() {
+        if (filePath != null) {
+
+
+
+            // Defining the child of storageReference
+            StorageReference ref
+                    = storageReference.child("images/" + UUID.randomUUID().toString());
+
+
+            ref.putFile(filePath)
+                    .addOnSuccessListener(
+                            new OnSuccessListener<UploadTask.TaskSnapshot>() {
+
+                                @Override
+                                public void onSuccess(
+                                        UploadTask.TaskSnapshot taskSnapshot) {
+
+                                    // Image uploaded successfully
+                                    // Dismiss dialog
+
+                                    Toast
+                                            .makeText(FIR.this,
+                                                    "Image Uploaded!!",
+                                                    Toast.LENGTH_SHORT)
+                                            .show();
+                                }
+                            })
+
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+
+                            // Error, Image not uploaded
+
+                            Toast
+                                    .makeText(FIR.this,
+                                            "Failed " + e.getMessage(),
+                                            Toast.LENGTH_SHORT)
+                                    .show();
+                        }
+                    })
+                    .addOnProgressListener(
+                            new OnProgressListener<UploadTask.TaskSnapshot>() {
+
+                                // Progress Listener for loading
+                                // percentage on the dialog box
+                                @Override
+                                public void onProgress(
+                                        UploadTask.TaskSnapshot taskSnapshot) {
+                                    double progress
+                                            = (100.0
+                                            * taskSnapshot.getBytesTransferred()
+                                            / taskSnapshot.getTotalByteCount());
+
+                                }
+                            });
+        }
+    }
+
+
+        public static String encoderfun(byte[] decval) {
         String conVal= android.util.Base64.encodeToString(decval, Base64.DEFAULT);
         return conVal;
     }
